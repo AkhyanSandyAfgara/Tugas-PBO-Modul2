d724acd4108fffb4bd6a10f9b3a5e87c0a6d65bc
Akhyan Sandy Afgara <akhyansandy@gmail.com>
2025-09-03 13:23:21 +0700

    Add files via upload

---
+++ prak2percobaan2/Address.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prak2percobaan2;
+
+/**
+ *
+ * @author AKHYAN
+ */
+class Address {
+    int strNum;
+    String city;
+    String state;
+    String country;
+    
+    Address(int street, String c, String st, String country){
+        this.strNum = street;
+        this.city = c;
+        this.state = st;
+        this.country = country;
+    }
+    
+}
---
+++ prak2percobaan2/Student.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prak2percobaan2;
+
+/**
+ *
+ * @author AKHYAN
+ */
+class Student {
+    int rno;
+    String stName;
+    
+    Address stAddr;
+    Student(int roll, String name, Address address){
+        this.rno = roll;
+        this.stName = name;
+        this.stAddr = address;
+                
+    }
+    
+}
