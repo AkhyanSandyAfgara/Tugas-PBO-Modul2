5b51f49a8b0cdd301ca449616ac7da5affc59421
Akhyan Sandy Afgara <akhyansandy@gmail.com>
2025-09-03 17:38:13 +0700

    Add files via upload

---
+++ skaggregation2/Material.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.skaggregation2;
+
+/**
+ *
+ * @author AKHYAN
+ */
+class Material {
+    String judul;
+
+    public Material(String judul) {
+        this.judul = judul;
+    }
+}
---
+++ skaggregation2/User.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.skaggregation2;
+
+/**
+ *
+ * @author AKHYAN
+ */
+class User {
+    String nama;
+
+    public User(String nama) {
+        this.nama = nama;
+    }
+}
